d3d5c1abeaeac812752a4fca6ac8b924194de195
allavinner <joel.w.ottosson@gmail.com>
2025-08-31 19:28:53 +0200

    INIT - SERVER

---
+++ server/app/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://localhost:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/hello")
+async def root():
+    return {"message": "Hello World"}
